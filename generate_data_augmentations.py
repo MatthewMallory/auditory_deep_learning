af5664ad5fbae55573ffaeef19d517fc047d8e10
matt <matt.mallory@alleninstitute.org>
2021-07-20 00:39:29 -0700

    updated spectrogram gen and aug

--- generate_data_augmentations.py
+++ generate_data_augmentations.py
@@ -3,45 +3,56 @@ import pandas as pd
 import librosa
 import tifffile
 import numpy as np
+from data_augmentation.augmentation import *
 import os
+import ntpath 
 
-def create_spectrogram_multiprocessing(track_path, mel_path, mel_db_path):
+def create_spectrogram_multiprocessing(track_path, mel_db_path):
 
-    try:
-        y, sr = librosa.load(track_path)
-        mel = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=2048,
-                                             hop_length=1024,
-                                             win_length=None,
-                                             window='hann',
-                                             center=True, pad_mode='reflect',
-                                             power=2.0) #,
-        mel_db = librosa.power_to_db(mel, ref=np.max)
-        tifffile.imwrite(mel_path, mel)
-        tifffile.imwrite(mel_db_path, mel_db)
-    except:
+    if True:#try:
+        audio_augmented = augment_audio(track_path)
+        
+        mel_db = tifffile.imread(mel_db_path)
+        spectrogram_augmented = augment_spectrogram(mel_db)
+
+        for aug_dict in [audio_augmented,spectrogram_augmented]:
+            for aug_label,aug_mel_db in aug_dict.items():
+                out_file = "{}_{}.tiff".format(mel_db_path[0:-5],aug_label)
+                tifffile.imwrite(out_file, aug_mel_db)
+                print(out_file)
+    else:
         print("FAILED")
         return track_path
 
 def main():
     print(librosa.__version__)
     parallel_inputs = []
-    for genre in os.listdir("mp3_files/"):
-        genre_dir = os.path.join("mp3_files", genre)
-        if os.path.isdir(genre_dir):
-            for track_id in [f for f in os.listdir(genre_dir) if ".mp3" in f]:
-                track_path = os.path.join(genre_dir, track_id)
-                mel_filename = track_id.split(".")[0] + '_mel_spect.tiff'
-                mel_db_filename = track_id.split(".")[0] + '_mel_spect_db.tiff'
+    for genre in os.listdir("/home/matt/audio_deep_learning/Data/gtzan_augmented/WAV_Files/"):
+        genre_dir = os.path.join("/home/matt/audio_deep_learning/Data/gtzan_augmented/WAV_Files", genre)
+        
 
-                mel_path = os.path.join(genre_dir, mel_filename)
-                mel_db_path = os.path.join(genre_dir, mel_db_filename)
-                if (not os.path.exists(mel_path)) or (not os.path.exists(mel_db_path)):
-                    parallel_inputs.append((track_path,mel_path,mel_db_path))
+        outdir = "/home/matt/audio_deep_learning/Data/gtzan_augmented/Spectrograms/"+genre
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+        
+        if os.path.isdir(genre_dir):
+            for track_id in [f for f in os.listdir(genre_dir) if ".wav" in f]:
+                track_path = os.path.join(genre_dir, track_id)
+
+                mel_db_filename = track_id.split(".wav")[0] + '_mel_spect_db.tiff'
+                mel_db_path = os.path.join(outdir, mel_db_filename)
+
+                if os.path.exists(mel_db_path):
+                    parallel_inputs.append((track_path,mel_db_path))
     
     p = Pool()
     print("There are {} tracks to analyze".format(len(parallel_inputs)))
+
     failed_tracks = p.starmap(create_spectrogram_multiprocessing,parallel_inputs)
-    pd.DataFrame({'tracks':failed_tracks}).to_csv("Failed Spectrogram.csv")
+    if failed_tracks != []:
+        pd.DataFrame({'tracks':failed_tracks}).to_csv("Failed Spectrogram.csv")
+    else:
+        print("Flawless")
 
 if __name__ == "__main__":
     main()
--- generate_spectrograms.py
+++ generate_spectrograms.py
@@ -1,11 +1,11 @@
 from multiprocessing import Pool
 import pandas as pd
-import librosa
-import tifffile
 import numpy as np
+import tifffile
+import librosa
 import os
 
-def create_spectrogram_multiprocessing(track_path, mel_path, mel_db_path):
+def create_spectrogram_multiprocessing(track_path, mel_db_path):
 
     try:
         y, sr = librosa.load(track_path)
@@ -16,7 +16,6 @@ def create_spectrogram_multiprocessing(track_path, mel_path, mel_db_path):
                                              center=True, pad_mode='reflect',
                                              power=2.0) #,
         mel_db = librosa.power_to_db(mel, ref=np.max)
-        tifffile.imwrite(mel_path, mel)
         tifffile.imwrite(mel_db_path, mel_db)
     except:
         print("FAILED")
@@ -25,18 +24,20 @@ def create_spectrogram_multiprocessing(track_path, mel_path, mel_db_path):
 def main():
     print(librosa.__version__)
     parallel_inputs = []
-    for genre in os.listdir("mp3_files/"):
-        genre_dir = os.path.join("mp3_files", genre)
-        if os.path.isdir(genre_dir):
-            for track_id in [f for f in os.listdir(genre_dir) if ".mp3" in f]:
-                track_path = os.path.join(genre_dir, track_id)
-                mel_filename = track_id.split(".")[0] + '_mel_spect.tiff'
-                mel_db_filename = track_id.split(".")[0] + '_mel_spect_db.tiff'
+    for genre in os.listdir(zz):
+        genre_dir = os.path.join("/home/matt/audio_deep_learning/Data/gtzan_raw/WAV_Files", genre)
+        outdir = "/home/matt/audio_deep_learning/Data/gtzan_raw/Spectrograms/"+genre
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
 
-                mel_path = os.path.join(genre_dir, mel_filename)
-                mel_db_path = os.path.join(genre_dir, mel_db_filename)
-                if (not os.path.exists(mel_path)) or (not os.path.exists(mel_db_path)):
-                    parallel_inputs.append((track_path,mel_path,mel_db_path))
+        if os.path.isdir(genre_dir):
+            for track_id in [f for f in os.listdir(genre_dir) if ".wav" in f]:
+                track_path = os.path.join(genre_dir, track_id)
+                
+                mel_db_filename = track_id.split(".wav")[0] + '_mel_spect_db.tiff'
+                mel_db_path = os.path.join(outdir, mel_db_filename)
+                if not os.path.exists(mel_db_path):
+                    parallel_inputs.append((track_path,mel_db_path))
     
     p = Pool()
     print("There are {} tracks to analyze".format(len(parallel_inputs)))
